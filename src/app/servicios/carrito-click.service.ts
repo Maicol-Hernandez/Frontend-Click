7c5dc424400c8a48e57238599f58defc733c9f37
Camilo-Chico <aguja.2003@gmail.com>
2021-05-17 17:47:02 -0500

    Se realizo correcciones en el carrito de compras y detalles

---
+++ src/app/servicios/carrito-click.service.ts
@@ -0,0 +1,130 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from "rxjs";
+import { IItem } from '../interfaces/item.interface';
+@Injectable({
+  providedIn: 'root'
+})
+export class CarritoClickService {
+
+  //Aqui vamos a guardar el array mediante un BehaviorSubject
+  public carritoUser : BehaviorSubject<Array<any>> = new BehaviorSubject([]);
+  public  pedidos = new BehaviorSubject<Array<IItem>>(null);
+  public sumCarritoUser : BehaviorSubject<Number> = new BehaviorSubject(null);
+  public listado :any = []
+  public carrito :any;
+  constructor() { }
+
+  //Metodo que nos permite editar el localStorage
+  setPedido(carrito : any=[]){
+    localStorage.setItem('carrito',JSON.stringify(carrito))
+  }
+  getPedidos():any{
+    return localStorage.getItem('carrito')
+  }
+
+  //inicializamos los datos ya guardados en el localStorage y los guardamos en BehaviorSubject
+  init(){
+    var local =JSON.parse(this.getPedidos())
+    //Si esta nulo el localStorage . lo configuramos.
+    if (local == null) {
+      this.setPedido(this.listado);
+      this.carritoUser.next(this.listado);
+      this.sumCarritoUser.next(0);
+    } else {
+    //Sino esta nulo guardamos los datos
+      this.listado = local;
+      this.carritoUser.next(this.listado);
+      this.sumCart();
+    }
+  }
+
+  //metodo para eliminar datos de un BehaviorSubject
+  delenteProduct(id:any){
+    this.listado = JSON.parse(this.getPedidos());
+    var i = 0;
+    for (const info  of this.listado) { 
+      //Si el id de info es igual al que estan registrados;
+      if(info.id == id){
+        //Elimine este dato
+           this.listado.splice(i,1)
+           console.log(this.listado)
+           //Guardamos los cambios en el localStorage
+           this.setPedido(this.listado);
+           this.carritoUser.next(this.listado);
+      }
+      i++    
+    }
+  }
+
+  //Buscar los productos repetidos 
+  add(producto:any,id): boolean{
+      var i = 0;
+      for (const select of this.listado) {
+        if(select.id == id){
+          producto.cantidad = select.cantidad;
+          var sum = producto.cantidad + 1;
+          producto.cantidad = sum;
+          this.listado[i] = producto;
+          //Guardar el listado 
+          this.carritoUser.next(this.listado);
+          this.setPedido(this.listado);
+          return false;
+        }
+        if(this.listado.length == 0){
+          //LLamamos la interfaz para tener un orden con interfaces
+          this.pedidos.next(producto);
+          //Obtenemos el valor la informacion
+          var info = this.pedidos.getValue();
+          this.listado.push(info);
+          this.carritoUser.next(this.listado);
+          this.setPedido(this.listado);
+          return false;
+        }
+        i++;
+      }
+      return true;
+}
+  //Metodo para agregar los productos al carrito
+  addProductCars(id:any,producto,cantidad){
+    var i = 0;
+    for (const add of producto){
+      var asignarProducto = add;
+        if(asignarProducto.id == id ){ 
+            //Seleccionamos el producto
+            this.carrito = asignarProducto;
+            //Agregamos un nuevo campo (cantidad) donde se va almecenar la cantidad
+            this.carrito.cantidad = cantidad;
+            //hacemos un objeto
+            producto = {  
+              id : this.carrito.id,
+              nombre : this.carrito.nombre,
+              precio : this.carrito.precio,
+              cantidad : this.carrito.cantidad,
+              foto : this.carrito.foto
+            }     
+            if(this.add(producto,id)){
+              this.pedidos.next(producto)
+              var info = this.pedidos.getValue();
+              this.listado.push(info);
+              this.carritoUser.next(this.listado);
+              this.setPedido(this.listado);
+            }
+            }
+          }
+      }
+
+  //Metodo que nos va permitir hacer la suma total del carrito
+  sumCart(){
+    var info :any=[];
+    var sum = 0;
+    for (const iterator of this.carritoUser.getValue()){
+      var multi = iterator.cantidad * iterator.precio;
+      info.push(multi);
+    }
+    for (let i = 0; i < info.length; i++) {
+      sum += info[i];
+    }
+    this.sumCarritoUser.next(sum);
+  }
+
+}
