05c4a65e92a3b0056f6b849bdadfa0a615928a7b
Camilo-Chico <aguja.2003@gmail.com>
2021-06-15 18:53:43 -0500

    Se organizaron partes del sistema

--- src/app/crear-producto/crear-producto.component.ts
+++ src/app/crear-producto/crear-producto.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { ClientService } from '../servicios/client.service';
 import { NegociosService } from '../servicios/negocios.service';
 import { Router } from '@angular/router';
-
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 @Component({
   selector: 'app-crear-producto',
   templateUrl: './crear-producto.component.html',
@@ -13,7 +13,7 @@ export class CrearProductoComponent implements OnInit {
 
   form: FormGroup;
   idNegocio;
-
+  infoImg;
   constructor(
     private fb: FormBuilder,
     private route: Router,
@@ -29,31 +29,72 @@ export class CrearProductoComponent implements OnInit {
     })
 
     this.form = this.fb.group({
-      idn: ['',Validators.required],
+      iva: ['',Validators.required],
       nombre: ['',Validators.required],
-      descripcion: ['',Validators.required],
       precio: ['',Validators.required],
-      logo: ['',Validators.required]
+      img: [null],
     })
   }
-
-  OnSubmit(){
-    if (this.form.valid) {
-      let data = {
-      idnegocio : this.form.value.idn,
-      nombre : this.form.value.nombre,
-      descripcion : this.form.value.descripcion,
-      precio : this.form.value.precio,
-      logo : this.form.value.logo
+  upload(event) {
+    if(event.target.files[0].type == 'image/jpg' || event.target.files[0].type == 'image/jpeg' || event.target.files[0].type == 'image/png'){
+      const file = (event.target as HTMLInputElement).files[0];
+      this.form.patchValue({
+        img: file
+      });
+      this.infoImg = event.target.files[0].name;
+      this.form.get('img').updateValueAndValidity()
+      var nombre = this.form.get('nombre').value
+      this.infoImg = nombre.trim();
+      var remplazo = this.infoImg.split(" ").join("")
+      this.infoImg = remplazo
+      }
     }
+
+
+  crearProducto(data:any){
     this.client.postRequestEnviarProductoCreado('http://localhost:5000/api/v02/user/crearProducto',data).subscribe(
       (response:any)=>{
-        console.log(response);
+        Swal.fire(
+          'Se aguardo correctamente el producto!',
+          'Tu producto se encuentra en su negocio',
+          'success'
+        )
+        this.route.navigate(['/zona-administracion']);
       },
       (error)=>{
-        console.log(error.status)
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'lo sentimos, No tenemos servicio'
+        })
+        this.route.navigate(['/zona-administracion']);
       }
     )
+  }
+  OnSubmit(){
+    if(this.form.valid){
+        var formData: any = new FormData();
+        formData.append("img", this.form.get('img').value);
+        formData.append("name", this.infoImg);
+        this.client.postRequestEnviarProductoCreado('http://localhost:8000/uploadProducto',formData).subscribe(
+        (response:any)=>{
+          let data = {
+            iva : this.form.value.iva,
+            idnegocio : this.idNegocio[0].id,
+            nombre : this.form.value.nombre,
+            precio : this.form.value.precio,
+            logo : response.img 
+            }
+            this.crearProducto(data);
+        },(error)=>{
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'lo sentimos, No se puede actualizar la imagen'
+          })
+        })
+    }else{
+      console.log("estado",false)
     }
   }
 }
