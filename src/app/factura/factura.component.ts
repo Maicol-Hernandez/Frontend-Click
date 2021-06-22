1d9b87b8676b0086d67ab2e051fd566a85d161dd
Miacol Hernandez <mfhernandez586@misena.edu.co>
2021-06-22 17:47:57 -0500

    se agrega registro pedidos

--- src/app/factura/factura.component.ts
+++ src/app/factura/factura.component.ts
@@ -7,9 +7,7 @@ import Swal from 'sweetalert2/dist/sweetalert2.js';
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 import { CarritoClickService } from '../servicios/carrito-click.service';
-import { element } from 'protractor';
-import { strict } from 'assert';
-import { stringify } from '@angular/compiler/src/util';
+
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-factura',
@@ -17,20 +15,19 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
   styleUrls: ['./factura.component.css']
 })
 export class FacturaComponent implements OnInit {
-  
+
   tablas = [];
-  paid : boolean = false;
-  pays : boolean = true;
-  total : any;
+  paid: boolean = false;
+  pays: boolean = true;
+  total: any;
   form: FormGroup;
   idNegocio;
   nombres;
   correo;
   numTelefono;
   id_usuario;
-  valorPedido;
   idPedido;
-  //datosDetalles: any = [];
+
 
   constructor(
     public carro: CarritoClickService,
@@ -41,59 +38,35 @@ export class FacturaComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    
-    this.client.getRequestPedirIdPedido('http://localhost:5000/api/v02/user/pedidoid').subscribe(
-      (data): any => {
-       this.idPedido = data['datos_pedido']
-       console.log("ID DEL PEDIDO: ", this.idPedido)
-      }, 
-      (error: any) => {
-        console.error(error)
 
-      });
-    
-    this.form = this.fb.group({
-      iva: ['', Validators.required]
-    });
-    
     this.carro.init();
-    
+
     this.carro.sumIva.getValue()
     this.carro.sumProducto.getValue()
     this.idNegocio = localStorage.getItem('id_negocio')
     this.nombres = localStorage.getItem('courrentUserNombres')
     this.correo = localStorage.getItem('courrentUserCorreo')
     this.numTelefono = localStorage.getItem('courrentUserNumeroTelefono')
-    this.id_usuario = localStorage.getItem('courrentUserIdUsuario')  
-/*
-    let data = {
-      iva: this.carro.sumIva.getValue(),
-      ValorTotal: this.carro.sumProducto.getValue(),
-     fecha: new Date().toLocaleString(),
-     id_negocio: this.idNegocio,
-     nombre: this.nombres,
-     correo: this.correo,
-     telefono: this.numTelefono,
-    }
-    console.log("Valor del data: ", data)
-*/
-    this.carro.listado.map(a =>{[
+    this.id_usuario = localStorage.getItem('courrentUserIdUsuario')
+
+    this.carro.listado.map(a => {
+      [
         this.tablas.push(
           {
             columns: [
               [
                 {
-                  text:a.nombre,
-                  bold:true,
+                  text: a.nombre,
+                  bold: true,
                 },
-              ],[
+              ], [
                 {
-                  text: a.cantidad , alignment: 'center' 
+                  text: a.cantidad, alignment: 'center'
                 },
               ],
               [
                 {
-                  text: '$ '+parseInt(a.precio) , alignment: 'right' 
+                  text: '$ ' + parseInt(a.precio), alignment: 'right'
                 },
               ]
 
@@ -105,125 +78,137 @@ export class FacturaComponent implements OnInit {
   }
 
 
+  async enviarPedido() {
 
- async enviarPedido() {
+    let data = {
+      iva: this.carro.sumIva.getValue(),
+      valorTotal: this.carro.sumProducto.getValue(),
+      fecha: new Date().toISOString(),
+      id_negocio: this.idNegocio,
+      id_usuario: this.id_usuario,
+    }
 
-  let data = {
-    iva: this.carro.sumIva.getValue(),
-    valorTotal: this.carro.sumProducto.getValue(),
-    fecha: new Date().toISOString(),
-    id_negocio: this.idNegocio,
-    id_usuario: this.id_usuario,
-  }
-  
-  //console.log("ESTE ES EL VALOR DE DATA:", data)
-  this.client.postRequestPedido('http://localhost:5000/api/v02/user/pedido', data).subscribe(
-    (response:any) => {
-  
-      Swal.fire({
-        title: 'Se pago correctamente',
-        imageUrl: 'https://media0.giphy.com/media/ZZYXNDxMcMDXIblV8L/source.gif',
-        imageWidth: 400,
-        imageHeight: 200,
+    this.client.postRequestPedido('http://localhost:5000/api/v02/user/pedido', data).subscribe(
+      (response: any) => {
+        let idPedido = response['id_pedido']
 
-      }).then(() => {
-        //this.route.navigate(['/detallesproducto'])
+        let valorPedido = this.carro.carritoUser.getValue()
+        var productosDetalles: any = []
+
+        for (let item of valorPedido) {
+          var iva = (item.precio * item.iva) / 100
+
+          let detalles = {
+            id: item.id,
+            nombre: item.nombre,
+            precio: item.precio,
+            cantidad: item.cantidad,
+            iva: iva,
+            idPedido: idPedido
+          }
+
+          console.log(`idPedido ${idPedido}`)
+          console.log(detalles)
+
+          productosDetalles.push(detalles)
+        }
+
+        let data = {
+          pedidoDetalles: productosDetalles
+        }
+
+        this.client.postRequestEnviarPedidoDetalles('http://localhost:5000/api/v02/user/pedidodetalles', data).subscribe(
+          (response: any) => {
+
+            Swal.fire({
+              title: 'Se pago correctamente',
+              imageUrl: 'https://media0.giphy.com/media/ZZYXNDxMcMDXIblV8L/source.gif',
+              imageWidth: 400,
+              imageHeight: 200,
+
+            }).then(() => {
+              //this.route.navigate(['/detallesproducto'])
+            });
+            console.log(response)
+
+          },
+          (error) => {
+            console.error(error);
+          });
+
+        //this.enviarPedidoDetalles(response['id_pedido'])
+
+        console.log(response)
+
+      },
+      (error) => {
+        console.error(error);
       });
 
-      console.log(response)
-    },
-    (error) => {
-      console.error(error);
-    });
-
   }
 
-
-  async enviarPedidoDetalles() {
-    this.valorPedido = this.carro.carritoUser.getValue()
-
-     var productosDetalles: any=[]
-    for (let item of this.valorPedido) {
-      
-      var iva = (item.precio * item.iva)/100
-      //console.group("Este es el valor del iva detalles :", iva)
-      
-      let detalles = {
-       id: item.id,
-       nombre: item.nombre,
-       precio: item.precio,
-       cantidad: item.cantidad,
-        iva: iva 
-      }
-
-      console.log(detalles)
-      productosDetalles.push(detalles)
-
-    }
-    
-    console.log(
-      `productosDetalles: ${productosDetalles} `
-      )  
-
-      let data = {
-        pedidoDetalles: productosDetalles
-      }      
-
-      console.log(`data: ${data}`) 
-
+  /*  async enviarPedidoDetalles(idPedido) {
+      console.log("este es el id del pedido: ", idPedido)
+  
       this.client.postRequestEnviarPedidoDetalles('http://localhost:5000/api/v02/user/pedidodetalles', data).subscribe(
         (response: any) => {
+  
+          Swal.fire({
+            title: 'Se pago correctamente',
+            imageUrl: 'https://media0.giphy.com/media/ZZYXNDxMcMDXIblV8L/source.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+  
+          }).then(() => {
+            //this.route.navigate(['/detallesproducto'])
+          });
           console.log(response)
+  
         },
         (error) => {
-          console.error(error)
-          
-        }
-      )
-
-
-    //console.log("Este es el valor del pedido:", this.valorPedido)
-    //console.log("Detalle del producto: ")
-  }
+          console.error(error);
+        });
   
-  pay(){
+    }*/
+
+  pay() {
     this.paid = true;
-    this.pays  = false;
+    this.pays = false;
   }
 
-  generatePdf(options){
-    let documentDefinition = { 
+  generatePdf(options) {
+    let documentDefinition = {
       content: [
-      {
-        text: 'PROYECTO CLICK',
-        fontSize: 16,
-        alignment: 'center',
-        color: '#0000',
-        margin: [0,30]
-      },
-      {
-        text: 'FACTURA DE LA COMPRA',
-        fontSize: 20,
-        bold: true,
-        alignment: 'center',
-        
-        color: 'skyblue'
-      },
-      {
-        text: 'Información',
-        style: 'sectionHeader',
-        margin: [0,20],
-      },
-      ,
+        {
+          text: 'PROYECTO CLICK',
+          fontSize: 16,
+          alignment: 'center',
+          color: '#0000',
+          margin: [0, 30]
+        },
+        {
+          text: 'FACTURA DE LA COMPRA',
+          fontSize: 20,
+          bold: true,
+          alignment: 'center',
+
+          color: 'skyblue'
+        },
+        {
+          text: 'Información',
+          style: 'sectionHeader',
+          margin: [0, 20],
+        },
+        ,
         {
           columns: [
             [
               {
                 text: `Nombre: ${this.nombres}`,
-                bold:true,
+                bold: true,
               },
-              { text: `Correo: ${this.correo}` ,bold:true,},
-              { text: `Numero: ${this.numTelefono}` ,bold:true,},
+              { text: `Correo: ${this.correo}`, bold: true, },
+              { text: `Numero: ${this.numTelefono}`, bold: true, },
             ],
             [
               {
@@ -235,52 +220,52 @@ export class FacturaComponent implements OnInit {
         },
         {
           text: 'Detalles de los productos',
-          margin: [0,10]
+          margin: [0, 10]
         },
         {
           columns: [
             [
               {
-                text:"Productos",
-                margin: [0,10],
+                text: "Productos",
+                margin: [0, 10],
                 fillColor: '#555555'
               },
-            ],[
+            ], [
               {
-                text: "Cantidad" ,
+                text: "Cantidad",
                 alignment: 'center',
-                margin: [0,10] 
+                margin: [0, 10]
               },
             ],
             [
               {
-                text: "Precio" ,
+                text: "Precio",
                 alignment: 'right',
-                margin: [0,10],
+                margin: [0, 10],
               },
             ]
-  
-          ] 
-        },    
-        this.tablas, 
+
+          ]
+        },
+        this.tablas,
         {
-          text: "I.V.A : $ "+this.carro.sumIva.getValue(),
-          margin: [50,60,0,0],
+          text: "I.V.A : $ " + this.carro.sumIva.getValue(),
+          margin: [50, 60, 0, 0],
           fontSize: 20,
         },
         {
-          text: 'Total : $ '+this.carro.sumCarritoUser.getValue(),
-          margin: [50,10],
+          text: 'Total : $ ' + this.carro.sumCarritoUser.getValue(),
+          margin: [50, 10],
           fontSize: 20,
         },
-        { qr: 'https://www.google.com/' ,fit: '50' },
+        { qr: 'https://www.google.com/', fit: '50' },
         {
           ul: [
             'El pedido se puede devolver en un máximo de 10 días .',
             'La garantía del producto estará sujeta a los términos y condiciones del fabricante.',
             'Esta es una factura generada por el sistema.',
           ],
-      }
+        }
       ],
     };
     switch (options) {
@@ -297,6 +282,6 @@ export class FacturaComponent implements OnInit {
         console.log("error");
         break;
     }
-   }
+  }
 
 }
