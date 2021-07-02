863d69f2fd51eb9a7100e377276cd22762797c0e
Mar Marin <marin.franco1604@gmail.com>
2021-07-01 23:46:40 -0500

    SE agrrega svelte

---
+++ Front_Click_Svelte/nosotros/src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	export let name="maria ";
+</script>
+
+<main id="fondo">
+      		<div id="imagen">
+        		<img src="../../Soft.png" alt="..." width="645px" height="645px">
+      		</div>
+
+		  
+
+      <div id="letra" class="card text-white bg-dark mb-3" style="max-width: 28rem;">
+  <div class="card-body">
+    <h4>¿Tienes problemas con tu software?</h4>
+    <p class="card-text text-center">Contactanos a través de :<br>
+	clickproyecto791@gmail.com</p>
+  </div>
+</div>
+       
+    
+</main>
+
+<style>
+	#imagen{margin :19px 0 0 30px;}
+	#letra {margin : -360px 100px -100px 60px;
+    float:right;
+	}
+
+
+	
+	
+	
+	
+	
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
